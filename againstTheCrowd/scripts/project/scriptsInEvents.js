2ea88adc47e0ff3aac48d8b4753d19de3a44dba6
GustavoChico <josegustavochico@gmail.com>
2024-05-08 09:19:26 +0200

    Folder test

---
+++ againstTheCrowd/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+import { callApi } from './main.js';
+
+
+const scriptsInEvents = {
+
+	async _main_Event109_Act9(runtime, localVars)
+	{
+		callApi(runtime);
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
